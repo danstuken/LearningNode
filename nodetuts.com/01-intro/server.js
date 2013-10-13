c352497fa164d31db681dfbad78cede06b3e81e7
Dan Kendall <danstuken@gmail.com>
2013-10-13 14:41:58 +0100

    Now the code is much more compact - no superficial/synthetic vars.

--- nodetuts.com/01-intro/server.js
+++ nodetuts.com/01-intro/server.js
@@ -1,13 +1,7 @@
-var http = require('http');
+require('http')
+	.createServer(function handleRequest(request, response){
+		response.writeHead(200, {'content-type': 'text/plain'})
+		response.end('hello world - compact');
+	})
+	.listen(8080);
 
-var server = http.createServer();
-
-function handleRequest(request, response){
-	response.writeHead(200, {'content-type': 'text/plain'})
-	response.write('hello world');
-	response.end();
-}
-
-server.on('request', handleRequest);
-
-server.listen(8080);
