a7616c93200d8db244b74bc6b4b12fefcfb7917d
Dan Kendall <danstuken@gmail.com>
2014-02-18 22:25:25 +0000

    'lecture' 5 - callbacks

--- udemy/node_plus_express/BeginningNode/Math/math.js
+++ udemy/node_plus_express/BeginningNode/Math/math.js
@@ -2,14 +2,17 @@
 var a = process.argv[2];
 var b = process.argv[3];
 
-var multiply = function(first, second){
-	var product = first * second;
-	return product;
+var multiply = function(first, second, callback){
+	console.log('multiply started');
+	setTimeout(function(){
+		var product = first * second;
+		console.log('multiply finished');
+		callback(product);
+	}, 2000)
 }
 
 var report = function(value){
 	console.log(value);
 }
 
-var result = multiply(a, b);
-report(result);
+var result = multiply(a, b, report);
