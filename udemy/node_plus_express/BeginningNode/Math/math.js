799602bf34e3ea71d8915611e8c4f6a1f18b2973
Dan Kendall <danstuken@gmail.com>
2014-02-18 22:02:38 +0000

    'lecture 2' complete

--- udemy/node_plus_express/BeginningNode/Math/math.js
+++ udemy/node_plus_express/BeginningNode/Math/math.js
@@ -2,5 +2,14 @@
 var a = process.argv[2];
 var b = process.argv[3];
 
-var result = a * b;
-console.log(result);
+var multiply = function(first, second){
+	var product = first * second;
+	return product;
+}
+
+var report = function(value){
+	console.log(value);
+}
+
+var result = multiply(a, b);
+report(result);
